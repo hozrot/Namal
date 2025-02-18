91c80525a3f26657e3a1b93360fd8d096516d394
hozrot <hozrot.ali64@gmail.com>
2025-02-18 16:56:07 +0600

    Dashboard work on progress

--- App.js
+++ App.js
@@ -1,177 +1,216 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from "react-native";
+import DuaCard from "./component/DuaCard";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <ScrollView>
-    <View style={styles.Topcontainer}>
-      <Text> Namal ! A practising Muslim </Text>
-      <StatusBar style="auto" />
-    </View>
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 11 </Text>
-      </TouchableOpacity>
-    </View>
-
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 10 </Text>
-      </TouchableOpacity>
-    </View>
-
-    <View style={styles.containerItem4}>
-      <View style={styles.header}>
-        <Text>Icon </Text>
-        <View style={{flex: 1,paddingLeft: 20}}>
-        <Text> Dua </Text>
-        
-        </View>
-        <Text>Share </Text>
+      <View style={styles.Topcontainer}>
+        <Text> Namal ! A practising Muslim </Text>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.ItemBody}>
-      <View style={styles.ItemContent}>
-        <Text style={styles.Arabic}>  اللهمّ لا سهلا إلا ما جالتاهو سهلا، وأنت تجالول هازنا إذا شئت سهلا </Text>
-        </View>
-        <View style={styles.ItemContent}>
-        <Text>Allahumma la sahla illa ma ja'altahu sahla, wa anta taj'alul hazna idha shi'ta sahla </Text>
-        </View>
-      <View style={styles.ItemContent}>
-        <Text>আল্লাহুম্মা লা সাহলা ইল্লা মা জা'আলতাহু সাহলা, ওয়া আন্তা তাজ'আলুল হাজনা ইদা শী'তা সাহলা </Text>
-        </View>
-      
-        <View style={styles.ItemContent}>
-        <Text>হে আল্লাহ, আপনি যা সহজ করেন তা ছাড়া আর কোন সহজ নেই এবং আপনি চাইলে কঠিনকে সহজ করতে পারেন। </Text>
-        </View>
-        </View>
-        <TouchableOpacity style={styles.ItemFooter}>
-          <Text>Reference: </Text>
-          <Text>More </Text>
+     
+        <TouchableOpacity style={{ flexDirection: "row",width: '100%',padding: 5,justifyContent:'center',alignItems:'center' }}>
+          <Image source={require('./assets/waste.png')} style={{width: 40, height: 40}} />
+          <Text style={{fontSize:20,fontWeight:'bold'}}> Ramadan Start in 12 Days </Text>
         </TouchableOpacity>
-         
-        
-    </View>
+   
 
-    <View style={styles.containerItem3}>
-      <TouchableOpacity>
-        <Text>Item 5 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem3}>
-      <TouchableOpacity>
-        <Text>Item 6 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem3}>
-      <TouchableOpacity>
-        <Text>Item 7 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 2 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 3 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 1 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem4}>
-      <TouchableOpacity>
-        <Text>Item 8 </Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.containerItem1}>
-      <TouchableOpacity>
-        <Text>Item 1 </Text>
-      </TouchableOpacity>
-    </View>
-    </ScrollView>
+      <View style={{ backgroundColor:'green',padding:5,margin:10} }>
+        <TouchableOpacity style={{ flexDirection: "row", justifyContent: 'space-between',width: '100%',padding: 5, }}>
+          <Text>AsmaulHusna </Text>
+          <View style={{ backgroundColor:'#209A8F',padding:5,borderRadius:10 }}>
+          <Text  style={{fontSize:20,fontWeight:'bold',color:'white'}}> 2 </Text>
+          </View>
+          
+          <Text> More.. </Text>
+        </TouchableOpacity>
+        <View style={{ justifyContent:'center',alignItems:'center',padding:5 }}>
+        <Text style={{fontSize:48,fontWeight:'bold'}}>الرحمن</Text>
+        <Text style={{fontSize:16,fontWeight:'heavy'}}>Ar Rahman</Text>
+        <Text style={{fontSize:16,fontWeight:'heavy'}}> আর রাহমান</Text>
+        <Text style={{fontSize:20,fontWeight:'heavy',textAlign:'center'}}>পরম করুণাময়, সবচেয়ে দয়ালু, কল্যাণময়</Text>
+        </View>
+      </View>
+
     
+
+      <ScrollView  horizontal showsHorizontalScrollIndicator={false} style={{ flexDirection: "row",padding: 5, }}>
+        <TouchableOpacity style={{  justifyContent:'center',alignContent:'center',alignItems:'center',padding:10,margin:5,backgroundColor:'tomato' }}>
+        <Image source={require('./assets/pray.png')} style={{width: 50, height: 50}} />
+          
+          <Text style={{ fontSize:16, fontWeight:'bold'}}> Salah </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{  justifyContent:'center',alignContent:'center',alignItems:'center',padding:10,margin:5,backgroundColor:'tomato' }}>
+        <Image source={require('./assets/kabaa.png')} style={{width: 50, height: 50}} />
+          
+          <Text style={{ fontSize:16, fontWeight:'bold'}}> Hazz </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{  justifyContent:'center',alignContent:'center',alignItems:'center',padding:10,margin:5,backgroundColor:'tomato' }}>
+        <Image source={require('./assets/moon.png')} style={{width: 50, height: 50}} />
+          
+          <Text style={{ fontSize:16, fontWeight:'bold'}}> Shaom </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{  justifyContent:'center',alignContent:'center',alignItems:'center',padding:10,margin:5,backgroundColor:'tomato' }}>
+        <Image source={require('./assets/iftar.png')} style={{width: 50, height: 50}} />
+          
+          <Text style={{ fontSize:16, fontWeight:'bold'}}> Jakat </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{  justifyContent:'center',alignContent:'center',alignItems:'center',padding:10,margin:5,backgroundColor:'tomato' }}>
+        <Image source={require('./assets/bismillah.png')} style={{width: 50, height: 50}} />
+          
+          <Text style={{ fontSize:16, fontWeight:'bold'}}> Kalima </Text>
+        </TouchableOpacity>
+      </ScrollView>
+      
+      <DuaCard
+           iconName={'book-open-variant'}
+            iconSize={36}
+            iconColor={"green"}
+            title={"Dua"}
+            arabic={"اللهمّ لا سهلا إلا ما جالتاهو سهلا، وأنت تجالول هازنا إذا شئت سهلا"}
+            english={"Allahumma la sahla illama ja'altahu sahla, wa anta taj'aa lul hazna eja shi'ta sahla"}
+            bangla={"আল্লাহুম্মা লা সাহলা ইল্লামা জা'আলতাহু সাহলা, ওয়া আন্তা তাজ'আ লুল হাজনা ইজা শী'তা সাহলা"}
+            meaning={"হে আল্লাহ, আপনি যা সহজ করেন তা ছাড়া আর কোন সহজ নেই এবং আপনি চাইলে কঠিনকে সহজ করতে পারেন।"}
+            reference={"Ibn Majah 1/335"}
+            onPress={() => console.log("DuaCard")}
+          />
+     
+      <View style={{justifyContent:'flex-start',padding:10,backgroundColor:'tomato',marginLeft:10,marginRight:10}}>
+       <Text style={{fontSize:20,fontWeight:'bold'}} >Upcoming Event </Text>
+       <View style={{flexDirection:'row',padding:5,flex:1}}>
+        <View style={{ backgroundColor:'lightblue',borderRadius:35,flex:.2,height:80,padding:5,justifyContent:'center',alignItems:'center' }}>
+        <Text style={{textAlign:'center',fontSize:14,fontWeight:'bold'
+        }}> 15 </Text>
+        <Text style={{textAlign:'center',paddingLeft:5,fontSize:16,fontWeight:'bold'
+        }}>Feb </Text>
+        </View>
+        <View style={{ padding:15,flex:.8 }}>
+        <Text style={{paddingLeft:5,fontSize:16,fontWeight:'bold'
+        }}>Start of Ramadan </Text>
+        <Text style={{paddingLeft:5,fontSize:14,fontWeight:'bold'}}>
+          2 March  </Text>
+          <Text style={{paddingLeft:5,fontSize:16,fontWeight:'condensedBold'}}>
+          13 Days Remaining  </Text>
+        </View> 
+      </View>
+      
+      <TouchableOpacity style={{justifyContent:'center',alignItems:'center'}}>
+          <Text style={{ color:'blue',fontWeight:'bold'}}>View All</Text>
+       </TouchableOpacity>
+       </View>
+      
+      <View style={styles.containerItem1}>
+        <TouchableOpacity>
+          <Text>Item 2 </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.containerItem1}>
+        <TouchableOpacity>
+          <Text>Item 3 </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.containerItem1}>
+        <TouchableOpacity>
+          <Text>Item 1 </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.containerItem4}>
+        <TouchableOpacity>
+          <Text>Item 8 </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.containerItem1}>
+        <TouchableOpacity>
+          <Text>Item 1 </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   Topcontainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
   header: {
-    flex: .1,
-    flexDirection: 'row',
+    flex: 0.1,
+    flexDirection: "row",
     padding: 5,
-    
   },
   ItemBody: {
-    flex: .8,
-    flexDirection: 'column',
-    backgroundColor: 'lightgreen',
-   
+    flex: 0.8,
+    flexDirection: "column",
+    backgroundColor: "lightgreen",
   },
   ItemContent: {
-    flexDirection: 'row',
-    backgroundColor: 'lightgreen',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    backgroundColor: "lightgreen",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 5,
   },
-  Arabic: { 
+  Arabic: {
     fontSize: 22,
-    color: 'red',
-    textAlign: 'center',
-
+    color: "red",
+    textAlign: "center",
   },
-  Bangla: { },
-  English: { },
+  Bangla: {},
+  English: {},
   ItemFooter: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 5,
-    backgroundColor: 'pink',
-    justifyContent: 'space-between',
-    width: '100%',
-    
+    backgroundColor: "pink",
+    justifyContent: "space-between",
+    width: "100%",
   },
 
   containerItem1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     height: 50,
-    backgroundColor: 'lightblue',
+    backgroundColor: "lightblue",
     margin: 10,
-  },  
+  },
   containerItem2: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     height: 80,
-    backgroundColor: 'lightblue',
+    backgroundColor: "lightblue",
     margin: 10,
   },
   containerItem3: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "center",
     height: 100,
-    backgroundColor: 'lightblue',
+    backgroundColor: "lightblue",
     margin: 10,
   },
   containerItem4: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    backgroundColor: 'lightblue',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    backgroundColor: "lightblue",
     margin: 10,
   },
 });
---
+++ component/DuaCard.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+export default function DuaCard({ title, arabic,bangla,english,meaning,reference, onPress, date, iconName, iconSize, amount,category,backgroundColor='#7FC56C',
+    iconColor = 'green' }) {
+    return (
+       <View style={styles.card}>
+               <View style={styles.header}>
+               <MaterialCommunityIcons name={iconName} size={iconSize} color={iconColor} />
+                 <View style={{ flex: 1, paddingLeft: 20 }}>
+                   <Text style={{fontSize:16,fontWeight:'bold'}}> {title} </Text>
+                   <Text style={{fontSize:12,fontWeight:'semibold'}}> {reference} </Text>
+                 </View>
+                 <TouchableOpacity>
+                     <MaterialCommunityIcons name="share-variant" size={24} color="black" />
+                 </TouchableOpacity>
+                
+               </View>
+               <View style={styles.ItemBody}>
+                 <View style={styles.ItemContent}>
+                   <Text style={styles.Arabic}>
+                     {arabic}
+                   </Text>
+                 </View>
+                 <View style={styles.ItemContent}>
+                   <Text>
+                   {english}
+                   </Text>
+                 </View>
+                 <View style={styles.ItemContent}>
+                   <Text>
+                    {bangla}
+                   </Text>
+                 </View>
+       
+                 <View style={styles.ItemContent}>
+                  <Text> {meaning}</Text>
+                 </View>
+               </View>
+               <View style={styles.ItemFooter}>
+                <TouchableOpacity>
+                <Text> Details.. </Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={{ flexDirection: "row" }}>
+                <Text> More. </Text>
+                    <MaterialCommunityIcons name="arrow-right" size={20} color="black" />
+                    <MaterialCommunityIcons name="arrow-right" size={20} color="black" />
+                </TouchableOpacity>
+
+               </View>
+             </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        backgroundColor: "lightblue",
+        margin: 10,
+      },
+    header: {
+        flex: 0.1,
+        flexDirection: "row",
+        padding: 5,
+      },
+      ItemBody: {
+        flex: 0.8,
+        flexDirection: "column",
+        backgroundColor: "lightgreen",
+      },
+      ItemContent: {
+        flexDirection: "row",
+        backgroundColor: "lightgreen",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+      },
+      Arabic: {
+        fontSize: 30,
+        color: "red",
+        textAlign: "center",
+      },
+      Bangla: {},
+      English: {},
+      ItemFooter: {
+        flex: 1,
+        flexDirection: "row",
+        padding: 5,
+        backgroundColor: "pink",
+        justifyContent: "space-between",
+        width: "100%",
+      },
+
+})
